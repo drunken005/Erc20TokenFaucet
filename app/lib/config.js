ff05cd5cf067d27430c560a5a37860fed32a7b43
xygeng <drunken@139.com>
2018-10-15 10:25:54 +0800

    Update config

--- app/lib/config.js
+++ app/lib/config.js
@@ -1,3 +1,3 @@
 module.exports.ethPoint = 'http://192.168.0.171:8545';
 module.exports.hostPoint = 'http://127.0.0.1:3000';
-module.exports.privateKey = '133AF55264AD0C3444E89094C130AE935C22988766D36A4697B4693B04D6A908';
+module.exports.privateKey = 'private key';
